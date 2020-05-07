0c48c6784188ee05438a9424504557e19d11add2
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-07 23:54:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
 array = ["kiwi"]
 def oxford_comma(array)
-  if array.length == 2
-    array[-2] << " and "
-  array.join
-  elsif array.length == 1
+  if array.length == 1
     array.join
   elsif array.length == 2
-    array.join(" and")
+    array.join(" and ")
   else
     array.join(", ")
+    array[-2] << " and "
   end
 end
 
